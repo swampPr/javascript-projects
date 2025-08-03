3589e10075b499d173a94c96bbce4da5d40f66c1
swampC <wolfwolf28wolf@gmail.com>
2025-08-03 12:37:00 -0400

    re-installing dependencies, gotta stop git ignoring my package.json

--- DSA/BST.js
+++ DSA/BST.js
@@ -1,72 +1,71 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.right = null;
-        this.left = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
 }
-
-
+//Test
 
 export class BST {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value, node = this.root) {
+    if (node === null) {
+      this.root = new Node(value);
+      return;
     }
 
-    insert(value, node = this.root) {
-        if (node === null) {
-            this.root = new Node(value);
-            return;
-        }
+    if (value === node) return;
 
-        if (value === node) return;
+    if (value < node.value) {
+      if (node.left === null) {
+        node.left = new Node(value);
+      }
+      this.insert(value, node.left);
+    } else if (value > node.value) {
+      if (node.right === null) {
+        node.right = new Node(value);
+      }
+      this.insert(value, node.right);
+    }
+  }
 
-        if (value < node.value) {
-            if (node.left === null) {
-                node.left = new Node(value);
-            }
-            this.insert(value, node.left);
-        } else if (value > node.value) {
-            if (node.right === null) {
-                node.right = new Node(value);
-            }
-            this.insert(value, node.right);
-        }
+  has(value, node = this.root) {
+    if (node === null) {
+      return false;
+    }
+    if (value === node.value) return true;
+    if (value < node.value) {
+      return this.has(value, node.left);
+    } else if (value > node.value) {
+      return this.has(value, node.right);
+    }
+  }
+
+  printTree(node = this.root) {
+    if (node === null) return;
+
+    this.printTree(node.left);
+    console.log(node.value);
+    this.printTree(node.right);
+  }
+
+  BFS() {
+    if (!this.root) return null;
+    let queue = [this.root];
+    let result = [];
+
+    while (queue.length) {
+      const node = queue.shift();
+      result.push(node);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
     }
 
-    has(value, node = this.root) {
-        if (node === null) {
-            return false;
-        }
-        if (value === node.value) return true;
-        if (value < node.value) {
-            return this.has(value, node.left);
-        } else if (value > node.value) {
-            return this.has(value, node.right);
-        }
-    }
-
-    printTree(node = this.root) {
-        if (node === null) return;
-
-        this.printTree(node.left);
-        console.log(node.value);
-        this.printTree(node.right);
-    }
-
-    BFS() {
-        if (!this.root) return null;
-        let queue = [this.root];
-        let result = [];
-
-        while (queue.length) {
-            const node = queue.shift();
-            result.push(node);
-
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
